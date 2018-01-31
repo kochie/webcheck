a7e63c7070caec81e81ff1029412ad1af9e69bb3
Robert Koch <robert@kochie.io>
2018-02-01 10:39:41 +1100

    Initial commit

---
+++ URLCheck.go
@@ -0,0 +1,11 @@
+package webcheck
+
+import (
+	"regexp"
+)
+
+URLregexp := regexp.MustCompile("[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
+
+func IsValidURL(url string) (bool) {
+	return URLregexp.MatchString(url)
+}
