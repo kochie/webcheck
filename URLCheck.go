b0bba5f53da8e6e533f6a14f43b8b3a9ff06d8fd
Robert Koch <robert@kochie.io>
2018-02-01 11:04:19 +1100

    Fixed some typos.

--- URLCheck.go
+++ URLCheck.go
@@ -4,7 +4,7 @@ import (
 	"regexp"
 )
 
-URLregexp := regexp.MustCompile("[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
+var URLregexp *regexp.Regexp = regexp.MustCompile("[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
 
 func IsValidURL(url string) (bool) {
 	return URLregexp.MatchString(url)
